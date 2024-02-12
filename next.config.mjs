a30f48b9b281b3dab92116dd211c191a5d361c70
Pedro <p20142004@gmail.com>
2024-02-12 11:27:01 -0300

    criando variavel de ambiente

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        URL_SOCKETIO: process.env.URL_SOCKETIO
+    }
+};
 
 export default nextConfig;
--- src/lib/socket-io.ts
+++ src/lib/socket-io.ts
@@ -1,2 +1,2 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:3333");
+export const socket = io(process.env.URL_SOCKETIO!);
